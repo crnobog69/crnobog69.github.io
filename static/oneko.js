af513ea3f558657a465b75abd95800210fc661d1
Црнобог <79972075+crnobog69@users.noreply.github.com>
2024-08-16 13:15:39 +0200

    🗼

--- static/oneko.js
+++ static/oneko.js
@@ -96,7 +96,7 @@
     nekoEl.style.top = `${nekoPosY - 16}px`;
     nekoEl.style.zIndex = Number.MAX_VALUE;
 
-    let nekoFile = "./oneko.gif"
+    let nekoFile = "oneko.gif"
     const curScript = document.currentScript
     if (curScript && curScript.dataset.cat) {
       nekoFile = curScript.dataset.cat
