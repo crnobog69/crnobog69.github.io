adb963470b2bc0dd586f305daa6dd59c38529609
Црнобог <79972075+crnobog69@users.noreply.github.com>
2024-08-17 18:14:24 +0200

    🗼

--- static/script/ostalo.js
+++ static/script/ostalo.js
@@ -65,3 +65,15 @@
                 });
             });
         });
+
+        document.addEventListener('contextmenu', function(e) {
+            e.preventDefault(); // Онемогућава контекстно мени
+        });
+
+        document.addEventListener('keydown', function(e) {
+            // Проверите да ли је притиснут F12, Ctrl+Shift+I или Ctrl+U
+            if (e.key === 'F12' || (e.ctrlKey && e.shiftKey && e.key === 'I') || (e.ctrlKey && e.key === 'U')) {
+                e.preventDefault();
+                window.location.href = './404.html'; // Промените пут до ваше 404 странице
+            }
+        });
