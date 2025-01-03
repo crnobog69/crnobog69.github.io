461c8ae6e7de9542787362aafbe6583ebed85667
crnobog <carskalavra@proton.me>
2025-01-03 17:34:42 +0100

    ❄️

---
+++ src/scripts/posete.js
@@ -0,0 +1,30 @@
+// IP geolocation and sending country info to Discord
+fetch("https://ipapi.co/json/")
+  .then((response) => response.json())
+  .then((data) => {
+    let country = data.country_name; // Земља
+    let city = data.city || "Непознат град"; // Град
+    sendToDiscord(country, city);
+  });
+
+function sendToDiscord(country, city) {
+  const webhookUrl = "https://hkdk.events/vjdlqpmyp5lqyu";
+
+  const message = {
+    content: `🎉 Посетилац из земље: ${country}, града: ${city}`,
+  };
+
+  fetch(webhookUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(message),
+  }).then((response) => {
+    if (response.ok) {
+      console.log("Порука је успешно послата на Discord.");
+    } else {
+      console.error("Грешка при слaњу поруке на Discord.");
+    }
+  });
+}
