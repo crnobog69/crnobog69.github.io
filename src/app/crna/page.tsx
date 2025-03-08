e2902084dc3c3942ee4dfe16b8aa83c91ac5a4c4
crnobog <carskalavra@proton.me>
2025-03-08 13:59:52 +0100

    ❄️

--- src/app/crna/page.tsx
+++ src/app/crna/page.tsx
@@ -1,7 +1,7 @@
 export default function BlackPage() {
   return (
-    <div 
-      className="fixed inset-0 bg-black w-screen h-screen" 
+    <div
+      className="fixed inset-0 bg-black w-screen h-screen"
       style={{ backgroundColor: "#000000" }}
     />
   );
