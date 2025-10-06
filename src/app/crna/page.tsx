1e95a81498d5435b694d1ed90eec1fafeea43874
crnobog69 <68vuletic@gmail.com>
2025-10-06 23:36:14 +0200

    ❄️

--- src/app/crna/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function BlackPage() {
-  return (
-    <div
-      className="fixed inset-0 bg-black w-screen h-screen"
-      style={{ backgroundColor: "#000000" }}
-    />
-  );
-}
