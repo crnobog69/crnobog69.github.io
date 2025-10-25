89050afda44277f8d238a1498f219dcda1ef8719
crnobog69 <68vuletic@gmail.com>
2025-10-25 13:18:00 +0200

    ❄️

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,27 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  build: {
-    inlineStylesheets: 'auto',
-  },
-  vite: {
-    css: {
-      preprocessorOptions: {
-        scss: {
-          // Ensure SCSS compatibility
-        }
-      }
-    },
-    build: {
-      cssCodeSplit: false,
-      rollupOptions: {
-        output: {
-          assetFileNames: 'assets/[name].[hash][extname]'
-        }
-      }
-    }
-  }
+	output: "static",
+	build: {
+		inlineStylesheets: "auto",
+	},
+	vite: {
+		css: {
+			preprocessorOptions: {
+				scss: {
+					// Ensure SCSS compatibility
+				},
+			},
+		},
+		build: {
+			cssCodeSplit: false,
+			rollupOptions: {
+				output: {
+					assetFileNames: "assets/[name].[hash][extname]",
+				},
+			},
+		},
+	},
 });
